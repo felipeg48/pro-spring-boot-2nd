b1d4842aaa4efd29d278605eeef1b52e37297d9d
felipeg48 <fgutierrezcruz@pivotal.io>
2018-03-18 12:40:39 -0600

    updated

--- ch02/spring-cli-apps/groovy-web/app.groovy
+++ ch02/spring-cli-apps/groovy-web/app.groovy
@@ -1,1 +1,8 @@
-@Controller class Web {}
+@RestController
+class WebApp{
+      @GetMapping("/")
+      String welcome(){
+         "<h1><font face='verdana'>Spring Boot Rocks!</font></h1>"
+      }
+}
+
