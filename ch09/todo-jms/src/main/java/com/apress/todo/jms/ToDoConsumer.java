ef9073a77dbc478c960dc998b28db2d61681010e
felipeg48 <fgutierrezcruz@pivotal.io>
2018-05-30 23:35:36 -0600

    updated

---
+++ ch09/todo-jms/src/main/java/com/apress/todo/jms/ToDoConsumer.java
@@ -0,0 +1,31 @@
+package com.apress.todo.jms;
+
+
+import com.apress.todo.domain.ToDo;
+import com.apress.todo.repository.ToDoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+
+import javax.validation.Valid;
+
+@Component
+public class ToDoConsumer {
+
+    private Logger log = LoggerFactory.getLogger(ToDoConsumer.class);
+    private ToDoRepository repository;
+
+    public ToDoConsumer(ToDoRepository repository){
+        this.repository = repository;
+    }
+
+    @JmsListener(destination = "${todo.jms.destination}",containerFactory = "jmsFactory")
+    public void processToDo(@Valid ToDo todo){
+        log.info("Consumer> " + todo);
+        log.info("ToDo created> " + this.repository.save(todo));
+    }
+}
